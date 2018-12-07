c55c5876bb946fe8f04933176ec11e832e694f34
Unknown <mauricio.koiazawa@datapar.com.py>
2018-12-07 11:43:31 -0300

    commit Ordenando, importando..

--- AngularFirebaseMaterial/src/app/app.module.ts
+++ AngularFirebaseMaterial/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CustomerListComponent } from './customer-list/customer-list.component';
     CustomerListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
