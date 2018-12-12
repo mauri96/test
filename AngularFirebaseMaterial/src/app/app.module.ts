e7418a1b93fdcec8789cb9c3fa49735217ae6aab
Unknown <mauricio.koiazawa@datapar.com.py>
2018-12-12 09:00:18 -0300

    commit

--- AngularFirebaseMaterial/src/app/app.module.ts
+++ AngularFirebaseMaterial/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
-    CustomerListComponent
   ],
   imports: [
     BrowserModule,
