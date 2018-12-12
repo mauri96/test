dea593d8e8f73861bf0fda602e6a2f3cf65c0e10
Unknown <mauricio.koiazawa@datapar.com.py>
2018-12-12 09:41:40 -0300

    configuracion, form

--- AngularFirebaseMaterial/src/app/app.module.ts
+++ AngularFirebaseMaterial/src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeService } from './shared/employee.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    EmployeeComponent,
+    EmployeesComponent,
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+    
     
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
