52bd1419175afce9ff034fbc1b10c8438f68fe96
Unknown <mauricio.koiazawa@datapar.com.py>
2018-12-13 12:25:21 -0300

    test firebase chat, configuracion, importacion de bootstrap, etc

---
+++ firechat/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {environment} from '../environments/environment';
+import { ChatComponent } from './components/chat/chat.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, AngularFireAuthModule,AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
